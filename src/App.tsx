e7cde79c7ab353aee899712387aafc79c03d8666
gustavomanca <gustavo.mancaa@gmail.com>
2024-03-12 17:32:32 -0300

    feat: add array type

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import './App.css'
 import { Button } from './components/ui/button'
 import { Textarea } from './components/ui/textarea'
 
-function shuffleArray(array) {
+function shuffleArray(array: string[]) {
   for (let i = array.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1))
     ;[array[i], array[j]] = [array[j], array[i]]
